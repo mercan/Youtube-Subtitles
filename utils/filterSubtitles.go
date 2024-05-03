5ffb36f938fde5adce7bf67aaeefe24f1e3d424d
mercan <imrcn77@gmail.com>
2024-05-03 18:56:09 +0300

    feat: refactor videoEmbedURL

--- utils/filterSubtitles.go
+++ utils/filterSubtitles.go
@@ -6,18 +6,17 @@ import (
 )
 
 func videoEmbedURL(videoId string, begin, end Time) string {
-	return fmt.Sprintf("https://www.youtube.com/embed/%s?start=%d&end=%d",
-		videoId,
-		begin.Hours*3600+begin.Minutes*60+begin.Seconds,
-		end.Hours*3600+end.Minutes*60+end.Seconds,
-	)
+	start := begin.Hours*3600 + begin.Minutes*60 + begin.Seconds
+	finish := end.Hours*3600 + end.Minutes*60 + end.Seconds
+
+	return fmt.Sprintf("https://www.youtube.com/embed/%s?start=%d&end=%d", videoId, start, finish)
 }
 
 func FilterSubtitles(videoId, text string, subtitles []Subtitle) []Subtitle {
-	filteredSubtitles := make([]Subtitle, 0, len(subtitles))	
-	
+	filteredSubtitles := make([]Subtitle, 0, len(subtitles))
+
 	re := regexp.MustCompile(`(?i)\b` + text + `\b`)
-	
+
 	for _, subtitle := range subtitles {
 		if re.MatchString(subtitle.Text) {
 			filteredSubtitles = append(filteredSubtitles, Subtitle{
